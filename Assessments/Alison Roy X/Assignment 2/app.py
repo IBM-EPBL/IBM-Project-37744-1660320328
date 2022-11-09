251fb1564cc2814f2967e9fb30e314cec3554ce5
Alison Roy <67859096+alisonroy@users.noreply.github.com>
2022-11-09 12:40:46 +0530

    Assignment 2 Alison

---
+++ Assessments/Alison Roy X/Assignment 2/app.py
@@ -0,0 +1,66 @@
+from flask import  Flask,render_template, request, redirect, url_for, session
+import ibm_db
+
+app = Flask(__name__)
+# Database Connection
+try:
+    conn = ibm_db.connect("DATABASE=BLUDB;HOSTNAME=125f9f61-9715-46f9-9399-c8177b21803b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=30426;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=rvm88889;PWD=nCp4LTEEPiIAsqpX",'','')
+    print("Connected to database: ", conn)
+except:
+    print ("Unable to connect to the database")
+    
+    
+@app.route('/',methods = ['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        rollnumber = request.form['rollnumber']
+        email = request.form['email']
+        password = request.form['password']
+        print(username,rollnumber,email,password)
+        
+        sql = "SELECT * FROM user WHERE username = '"+username+"' "
+        print(sql)
+        stmt = ibm_db.exec_immediate(conn, sql)
+        
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+
+        if account:
+            print("User already exists")
+            return render_template('register.html')
+        else:
+            print("User does not exist")
+            insert_query = "INSERT INTO user VALUES('"+email+"','"+password+"','"+rollnumber+"','"+username+"')"
+            ibm_db.exec_immediate(conn, insert_query)
+            print("You are successfully registered")
+            return render_template('login.html')
+    return render_template('register.html')
+
+if __name__ == "main":
+    app.run(debug=True)
+    
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    print("Login")
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+      
+        print(email,password)
+        sql = "SELECT password FROM user WHERE email = '"+email+"' "
+        print(sql)
+        stmt = ibm_db.exec_immediate(conn, sql)
+        print(stmt)
+        pwd = ibm_db.fetch_assoc(stmt)
+        print(pwd)
+        key = pwd.get('PASSWORD')
+
+        if password==key.strip():
+            print("User exists")
+            return render_template('home.html')
+        else:
+            print("User does not exist")
+            return render_template('register.html')
+    return render_template('login.html')
